a612b92d87ee693a19b44ac12d0c98f9e5cc7f05
chao meng <chaomeng@chaodeMacBook-Pro.local>
2022-03-27 19:35:46 +0800

    thread pool

--- 01.example/10.test_thread_pool/src/main.cpp
+++ 01.example/10.test_thread_pool/src/main.cpp
@@ -12,59 +12,6 @@ using namespace std;
 
 // telnet 127.0.0.1 20010
 
-void ReadCB(struct bufferevent *bev, void *ctx)
-{
-    char buf[1024] = {0};
-    int len = bufferevent_read(bev, buf, sizeof(buf) - 1);
-    cout << "read len = " << len << endl;
-    cout << buf << endl;
-
-    // 插入buffer链表，监听到sock可写，才去写
-    bufferevent_write(bev, "OK", 3);
-}
-
-void WriteCB(struct bufferevent *bev, void *ctx)
-{
-    cout << "write CB" << endl;
-}
-
-void EventCB(struct bufferevent *bev, short what, void *ctx)
-{
-    cout << "eventCB" << endl;
-    if (what & BEV_EVENT_TIMEOUT && what & BEV_EVENT_READING)
-    {
-        cout << "Read Timeout" << endl;
-        //读取缓冲区的内容
-
-        //然后关掉
-        bufferevent_free(bev);
-    }
-    else if (what & BEV_EVENT_TIMEOUT && what & BEV_EVENT_WRITING)
-    {
-        cout << "Write Timeout" << endl;
-        //缓冲回滚
-
-        //然后关掉
-        bufferevent_free(bev);
-    }
-    else if (what & BEV_EVENT_ERROR)
-    {
-        cout << "BEV_EVENT_ERROR" << endl;
-
-        //关掉
-        bufferevent_free(bev);
-    }
-    // 关闭连接的正常错误
-    else if (what & BEV_EVENT_EOF)
-    {
-        cout << "BEV_EVENT_EOF" << endl;
-        // 考虑缓冲的处理
-
-        //关掉
-        bufferevent_free(bev);
-    }
-}
-
 void ListenCB(struct evconnlistener *e, evutil_socket_t s,
               struct sockaddr *client_addr,
               int socklen, void *arg)
@@ -83,7 +30,7 @@ int main()
     }
 
     // 1、初始化线程池
-    XThreadPool::Get()->Init(10); // 创建十个线程
+    XThreadPool::Get()->Init(3); // 创建3个线程
     cout << "test thread pool create success" << endl;
 
     // 创建libevent 上下文，默认是创建base锁
